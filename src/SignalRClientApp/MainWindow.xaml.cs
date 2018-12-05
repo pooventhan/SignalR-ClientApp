53670a11add462a425bd77c67e0e57ee1e5de3c4
Pooventhan Supramaniam <pooventhan.supramaniam@my.experian.com>
2018-12-05 17:32:31 +0800

    Initial commit.

---
+++ src/SignalRClientApp/MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Windows;
+using Microsoft.AspNetCore.SignalR.Client;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SignalRClientApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private HubConnection connection;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            var endpoint = endpointValue.Text.Trim();
+            var authToken = authTokenValue.Text.Trim();
+
+            if (this.connection == null)
+            {
+                WriteLogOuput("Connecting...");
+
+                this.connection = BuildHubConnection(endpoint, authToken);
+
+                ConfigureResponseSubscription();
+
+                try
+                {
+                    await this.connection.StartAsync();
+                    WriteLogOuput("Connected succesfully!");
+                }
+                catch (Exception ex)
+                {
+                    WriteLogOuput(ex.Message);
+                }
+            }
+        }
+
+        private async void DisconnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.connection != null)
+            {
+                WriteLogOuput("Disconnecting...");
+
+                await connection.StopAsync().ConfigureAwait(false);
+                await connection.DisposeAsync().ConfigureAwait(false);
+                connection = null;
+
+                WriteLogOuput("Disconnected.");
+            }
+        }
+
+        private async void SendRequestButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.connection == null)
+            {
+                WriteLogOuput("Please connect to an endpoint before sending request.");
+                return;
+            }
+
+            var request = JsonConvert.DeserializeObject(requestValue.Text);
+
+            try
+            {
+                WriteLogOuput("Sending request..");
+                SetResponseOutput(string.Empty);
+                await connection.InvokeAsync("ValidateEmail", request).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                WriteLogOuput(ex.Message);
+            }
+        }
+
+        private HubConnection BuildHubConnection(string endpoint, string authToken)
+        {
+            return new HubConnectionBuilder()
+                .WithUrl(endpoint, options =>
+                {
+                    options.Headers.Add("Auth-Token", authToken);
+                })
+                .Build();
+        }
+
+        private void ConfigureResponseSubscription()
+        {
+            connection.On<object>("OnEmailResponse", (response) =>
+            {
+                WriteLogOuput("Response incoming..");
+
+                var jsonObject = response as JObject;
+
+                SetResponseOutput(jsonObject.ToString());
+            });
+        }
+
+        private void WriteLogOuput(string message)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                var getCurrentTime = DateTime.Now.ToString("HH:mm:ss");
+                logsOuput.AppendText($"{getCurrentTime}: {message}{Environment.NewLine}");
+            });
+        }
+
+        private void SetResponseOutput(string output)
+        {
+            this.Dispatcher.Invoke(() => 
+            {
+                responseValue.Text = output;
+            });
+        }
+    }
+}
